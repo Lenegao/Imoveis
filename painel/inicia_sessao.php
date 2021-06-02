03b3a1871d7dcaf2c45395b042cebe5ec5264b3c
Lenegao <85140948+Lenegao@users.noreply.github.com>
2021-06-02 13:32:45 -0300

    Delete painel directory

--- painel/inicia_sessao.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SESSION["S_autenticado"] != 'true') {
-	print "<script> window.location.href = 'index.php'; </script>";
-	exit();
-}
-$login = $_SESSION["S_login"];
-?>

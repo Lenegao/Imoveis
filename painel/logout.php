d131ce56c5f559cdc2de679207272d090e302fa1
Lenegao <85140948+Lenegao@users.noreply.github.com>
2021-06-02 13:45:25 -0300

    Update logout.php

--- painel/logout.php
+++ painel/logout.php
@@ -2,4 +2,4 @@
 session_start();
 $_SESSION["S_autenticado"]=false;
 ?>
-<script> window.location.href = 'http://<?php print $_SERVER['SERVER_NAME']; ?>'; </script>
+<script> window.location.href = 'http://<?php print $_SERVER['SERVER_NAME']; ?>/testes/api/painel/'; </script>
